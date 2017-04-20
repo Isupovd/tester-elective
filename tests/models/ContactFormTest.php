7c3c8c087124d9939ecac449e4f82b620bd5a155
Isupov_Dmitry <isupov_d@ekb.sima-land.ru>
2017-04-20 10:08:01 +0500

    fix require

--- tests/models/ContactFormTest.php
+++ tests/models/ContactFormTest.php
@@ -1,13 +1,13 @@
 <?php
-namespace Isupovd\tester-elective\tests
+//namespace Isupovd\tester-elective
 
-//require('../../models/ContactForm.php');
+require('../../src/models/ContactForm.php');
 
 use PHPUnit\Framework\TestCase;
 
 class ContactFormTest extends TestCase
 {
-    public function CreateObjectTest()
+    public function testCreateObject()
     {
         $object = new ContactForm();
         echo 'PASSED';
